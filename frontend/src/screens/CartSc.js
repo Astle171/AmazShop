a83d98a286576b21f28e52c0b0f0a03099fdb760
Astle171 <astle1999machado@gmail.com>
2022-09-28 03:11:54 +0530

    added cart screen

--- frontend/src/screens/CartSc.js
+++ frontend/src/screens/CartSc.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message.js'
+import { Link, useParams, useNavigate, useSearchParams } from 'react-router-dom'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { addToCart } from '../actions/cartActions'
 
 export const CartSc = () => {
+  const params = useParams()
+
+  const productId = params.id
+
+  const [searchParams] = useSearchParams()
+
+  const qty = searchParams.get('qty') ? Number(searchParams.get('qty')) : 1
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  console.log(cartItems)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
   return <div>Cart Screen</div>
 }
