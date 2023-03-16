7da28564aede6c23e13e504404b61a7421f75bb3
Astle171 <astle1999machado@gmail.com>
2023-03-17 01:52:11 +0530

    added sign up and login page, also added profile page for updatation of details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 import { CartSc } from './screens/CartSc.js'
 
 const App = () => {
@@ -14,10 +17,13 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/cart' element={<CartSc />} />
             <Route path='/cart/:id' element={<CartSc />} />
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/login' element={<LoginScreen />} />
           </Routes>
         </Container>
       </main>
